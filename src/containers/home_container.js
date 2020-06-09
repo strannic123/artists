8a7f5fe9040e609d455a2bc96950959a0f1a1fdb
str <manager350@mail.ru>
2020-06-09 16:21:19 +0800

    добавлен сервер/реализованы запросы и вывод артистов

---
+++ src/containers/home_container.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import  { connect } from "react-redux";
+import { artistListAll } from "../actions";
+import { bindActionCreators } from "redux";
+
+import Search from '../components/search';
+import Artistlist from '../components/artistlist';
+
+class HomeContainer extends Component {
+
+  componentWillMount() {
+    console.log(this.props.data);
+    this.props.artistListAll();
+  }
+
+
+  getKeywords = (event) => {
+    let key = event.target.value;
+
+  }
+
+  render(){
+    console.log(this.props)
+    return (
+      <div>
+        <Search keywords={this.getKeywords}/>
+        <Artistlist artists={this.props.artists.artistList}/>
+      </div>
+    )
+  }
+
+}
+
+function mapStateToProps(state) {
+  return {
+    artists: state.artists
+  }
+}
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({artistListAll}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeContainer);
